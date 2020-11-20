3cfb1f713d4038ce3200aa2f2349472ec3db40ed
GabrielBaezB <gabooo@live.com>
2020-11-19 23:21:54 -0300

    Se soluciono el ingreso y validaciones del rut

--- Fuentes/app/src/main/java/cl/inacap/registrocovid/MainActivity.java
+++ Fuentes/app/src/main/java/cl/inacap/registrocovid/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText idUsuario, passUsuario;
     private Button btnLoginUser;
 
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
     }
 
@@ -33,40 +33,38 @@ public class MainActivity extends AppCompatActivity {
         btnLoginUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String user =  idUsuario.getText().toString().trim();
+                String user = idUsuario.getText().toString().trim();
                 String pass = passUsuario.getText().toString().trim();
-                try{
-                    if (user.isEmpty()){
-                        Toast.makeText(MainActivity.this, "Ingrese nombre de Usuario"
-                                , Toast.LENGTH_SHORT).show();
-                    }else if (validarRut(user) == false) {
-                        Toast.makeText(MainActivity.this, "Ingrese una Cuenta Valida"
-                                , Toast.LENGTH_SHORT).show();
-                    }if (validarRut(user)== true){
+                //obtener el largo del rut
+                int digitosUser = user.length();
+                //obtener los ultimos 4 digitos del rut sin digito verificador
+                String passUser = user.substring(digitosUser -6, digitosUser-2);
+                int guion = user.indexOf(digitosUser-1);
+              if (!user.isEmpty()){
+                  if (!pass.isEmpty()){
+                      //validar rut
+                      if (validarRut(user) && user.contains("-")){
+                          //validar contraseña
+                          if (pass.equalsIgnoreCase(passUser)){
+                            //iniciar MainActivity
+                              startActivity(new Intent(MainActivity.this, PrincipalActivity.class));
 
-                        if (pass.isEmpty()){
-                        Toast.makeText(MainActivity.this, "Debe ingresar contraseña"
-                                , Toast.LENGTH_SHORT).show();
-
-                        }else if (user.length() == 10) {
-                            if (pass.equals(user.substring(4, 8))) {
-                                startActivity(new Intent(MainActivity.this, PrincipalActivity.class));
-                            } else {
-                                Toast.makeText(MainActivity.this, "Ingrese un Usuario Valido"
-                                        , Toast.LENGTH_SHORT).show();
-                            }
-                        }else if(user.length() == 9){
-                            if (pass.equals(user.substring(3,7))){
-                            startActivity(new Intent(MainActivity.this, PrincipalActivity.class));
-                        }else {
-                                Toast.makeText(MainActivity.this, "Contraseña Incorrecta", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    }
-            }catch (Exception e){
-
-                }
-
-        }});}}
+                          } else{
+                              Toast.makeText(MainActivity.this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show();
+                          }
+                      } else{
+                          Toast.makeText(MainActivity.this, "Ingrese rut válido", Toast.LENGTH_SHORT).show();
+                      }
+                  } else {
+                      Toast.makeText(MainActivity.this, "Ingrese contraseña de Usuario", Toast.LENGTH_SHORT).show();
+                  }
+              } else{
+                  Toast.makeText(MainActivity.this, "Ingrese nombre de Usuario", Toast.LENGTH_SHORT).show();
+              }
+              
+            }
+        });
+    }
+}
 
 
